5de2df7d084436b6edfe5a1909e943182f4e334f
Anagha Dudihalli <anaghadudihalli@gmail.com>
2017-09-16 09:35:21 +0000

    Add bot response

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -21,13 +21,22 @@ app.controller('chatController',function($scope){
             time: "1:14PM"
         }
     ];
+    
 
+    
 
     var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
-
-    exampleSocket.onmessage = function  (event) {
-           var jsonData = JSON.parse(event.data);
-           console.log(jsonData);
+        exampleSocket.onmessage  =   function  (event) {        
+        var jsonData = JSON.parse(event.data);        
+        jsonData.time = new Date().toLocaleTimeString();        
+        $scope.messages.push(jsonData);     //pushing the data   
+        $scope.$apply();                    //refreshing
+        console.log(jsonData);
+        };
+    
+    $scope.sendMessage = function () {            
+        exampleSocket.send($scope.userMessage);     //Sends user message     
+        $scope.userMessage = "";        //Clear the messaging area
     };
 
 });

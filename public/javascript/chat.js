3d97bfe00e209d0998ffdb356fd7e021d8b74a85
Anagha Dudihalli <anaghadudihalli@gmail.com>
2017-09-16 11:41:51 +0000

    Update heroku link

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -7,7 +7,7 @@ app.controller('chatController',function($scope, $sce){
 
     
 
-    var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+    var  exampleSocket =  new  WebSocket("wss://swiftcode-newschatbot.herokuapp.com");
         exampleSocket.onmessage  =   function  (event) {        
         var jsonData = JSON.parse(event.data);        
         jsonData.time = new Date().toLocaleTimeString();        
